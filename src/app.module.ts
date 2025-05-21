bbc17041a56e032f7799d51a2686f9f3893e3dd2
srxrt <str9703@gmail.com>
2025-05-21 12:47:10 +0900

    feat: create user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './components/user/user.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), DatabaseModule],
+  imports: [ConfigModule.forRoot(), DatabaseModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/schemas/User.schema.ts
+++ src/schemas/User.schema.ts
@@ -1,90 +1,60 @@
 import { Schema } from 'mongoose';
-import { UserAuthType, UserStatus, UserType } from '../libs/enums/member.enum';
+import { UserAuthType, UserStatus, UserType } from '../libs/enums/user.enums';
 
 const UserSchema = new Schema(
   {
-    memberType: {
+    userType: {
       type: String,
       enum: UserType,
       default: UserType.USER,
     },
-    memberStatus: {
+    userStatus: {
       type: String,
       enum: UserStatus,
       default: UserStatus.ACTIVE,
     },
-    memberAuthType: {
+    userAuthType: {
       type: String,
       enum: UserAuthType,
-      default: UserAuthType.PHONE,
+      default: UserAuthType.TELEGRAM,
     },
-    memberPhone: {
+    userPhone: {
+      type: String,
+      index: { unique: true, sparse: true },
+    },
+    userNick: {
       type: String,
       index: { unique: true, sparse: true },
       required: true,
     },
-    memberNick: {
-      type: String,
-      index: { unique: true, sparse: true },
-      required: true,
-    },
-    memberPassword: {
+    userPassword: {
       type: String,
       select: false,
       required: true,
     },
-    memberFullName: {
+    userFullName: {
       type: String,
     },
-    memberImage: {
+    userImage: {
       type: String,
       default: '',
     },
-    memberAddress: {
+    userAddress: {
       type: String,
     },
-    memberDesc: String,
-    memberProperties: {
+    userRestaurants: {
       type: Number,
       default: 0,
     },
-    memberArticles: {
+    userPoints: {
       type: Number,
       default: 0,
     },
-    memberFollowers: {
+    userRank: {
       type: Number,
       default: 0,
     },
-    memberFollowings: {
-      type: Number,
-      default: 0,
-    },
-    memberPoints: {
-      type: Number,
-      default: 0,
-    },
-    memberLikes: {
-      type: Number,
-      default: 0,
-    },
-    memberViews: {
-      type: Number,
-      default: 0,
-    },
-    memberComments: {
-      type: Number,
-      default: 0,
-    },
-    memberRank: {
-      type: Number,
-      default: 0,
-    },
-    memberWarnings: {
-      type: Number,
-      default: 0,
-    },
-    memberBlocks: {
+    userBlocks: {
       type: Number,
       default: 0,
     },
