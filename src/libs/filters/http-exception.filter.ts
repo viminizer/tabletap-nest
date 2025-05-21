d2bad8797397b8a11cc52c5b3b42bff3aad111c2
srxrt <str9703@gmail.com>
2025-05-21 12:55:15 +0900

    feat: create exception filter

---
+++ src/libs/filters/http-exception.filter.ts
@@ -0,0 +1,24 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+} from '@nestjs/common';
+import { Response } from 'express';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const status = exception.getStatus();
+    const responseBody = exception.getResponse();
+    const message =
+      typeof responseBody === 'string' ? responseBody : responseBody['message'];
+
+    response.status(status).json({
+      message: typeof message === 'string' ? message : message[0],
+      statusCode: status,
+    });
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './libs/filters/http-exception.filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3003);
 }
 bootstrap();
